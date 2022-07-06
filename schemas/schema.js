b97b546d5cc7e68fe5674d457dfce6516a50f86d
S A HARIPRASAD <myharicode@gmail.com>
2022-07-06 17:02:46 +0530

    schema setup without authentication complete

--- schemas/schema.js
+++ schemas/schema.js
@@ -17,21 +17,55 @@ const ClientType = new GraphQLObjectType({
 	})
 });
 
-// Root query for getting information with iD
+// Project Type
+const ProjectType = new GraphQLObjectType({
+	name:'Project',
+	fields:()=>({
+		id:{type:GraphQLID},
+		name:{type:GraphQLString},
+		description:{type:GraphQLString},
+		status:{type:GraphQLString},
+		client:{
+			type:ClientType,
+			resolve(parent,args){
+				return clients.find(client => client.id === parent.clientId)
+			}
+		}
+	})
+})
+
+// Root query for getting information 
 const RootQuery = new GraphQLObjectType({
 	name:'RootQueryType',
 	fields:{
+		// Fetching all projects
+		projects:{
+			type:new GraphQLList(ProjectType),
+			resolve(parent,args){
+				return projects
+			}
+		},
+		// Fetching all clients
 		clients:{
 			type:new GraphQLList(ClientType),
 			resolve(parent,args){
 				return clients
 			}
 		},
+		// fetching client by id
 		client:{
 			type:ClientType,
 			args:{id:{type:GraphQLID}},
 			resolve(parent,args){
 				return clients.find(client => client.id === args.id)
+			}
+		},
+		// fetching project by id
+		project:{
+			type:ProjectType,
+			args:{id:{type:GraphQLID}},
+			resolve(parent,args){
+				return projects.find(project=>project.id === args.id)
 			}
 		}
 	}
