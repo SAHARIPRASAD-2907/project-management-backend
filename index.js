c683d2dc4a345a7ef5e39c3dc91cc58868099ae6
S A HARIPRASAD <myharicode@gmail.com>
2022-07-06 12:49:42 +0530

    read query setup and tested

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-//console.log("Hello working");
+// Importing modules
 const express = require("express");
+const { graphqlHTTP } = require('express-graphql');
 require("dotenv").config();
+const schema = require("./schemas/schema")
 
 const port = process.env.PORT || 8000;
 
+// Setting up express server
 const app = express();
+
+// Setting up graphQL endpoints
+app.use('/graphql',
+	graphqlHTTP({
+	schema,
+	graphiql: process.env.NODE_ENV === "development"
+}))
+
 
 app.listen(port, console.log(`server running on port ${port}`));
