3cdbf5a126bb011124a8393a793f969fd411d97a
S A HARIPRASAD <myharicode@gmail.com>
2022-07-06 18:39:29 +0530

    connecting of database with localhost compleated

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 // Importing modules
 const express = require("express");
+const colors = require('colors')
 const { graphqlHTTP } = require('express-graphql');
 require("dotenv").config();
 const schema = require("./schemas/schema")
-
+const connectDB = require('./config/db')
 const port = process.env.PORT || 8000;
 
 // Setting up express server
 const app = express();
+
+// connecting to mongoDB database
+connectDB()
 
 // Setting up graphQL endpoints
 app.use('/graphql',
